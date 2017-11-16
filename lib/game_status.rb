ff9fde448fa48265d36246e79d8f8571ae967530
Taylor Donlan <taylordonlan@gmail.com>
2017-11-16 20:39:30 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -24,7 +24,7 @@ def won?(board)
     position_2 = board[win_index[1]]
     position_3 = board[win_index[2]]
     if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
-      puts "Games won in position: #{win_index}"
+      puts "Game won in position: #{win_index}"
       return win_index
     else
       false
