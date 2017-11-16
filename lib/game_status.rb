bb4406431a676285c9c653ba5c3797a03e1b7a44
Taylor Donlan <taylordonlan@gmail.com>
2017-11-16 21:54:04 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -28,10 +28,10 @@ def won?(board)
         position_3 = board[win_combination[2]]
         if (position_1 == "X" && position_2 == "X" && position_3 == "X")
           #  puts "Game won in position: #{win_index}"
-          winner = "X"
+          winner = X
           return win_combination
         elsif  (position_1 == "O" && position_2 == "O" && position_3 == "O")
-          winner = "O"
+          winner = O
           return win_combination
           #  puts "Game won in position: #{win_index}"
 
