e7552775e5723bb709c3e6f4b1b37dd28523e6ad
Taylor Donlan <taylordonlan@gmail.com>
2017-11-16 20:16:45 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -21,10 +21,12 @@ WIN_COMBINATIONS = [
 def won?(board)
   WIN_COMBINATIONS.each do |win_index|
 
-
+    if board.values_at(win_index).count("X") == 3
       puts "Game won at position: #{win_index}"
-
-  end
+    else
+      return false
+    end
+end
 end
 
 
