7f95d434502d340f7a8088894cd16c262fcc94da
Taylor Donlan <taylordonlan@gmail.com>
2017-11-16 23:05:20 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -32,7 +32,7 @@ def won?(board)
         elsif  (position_1 == "O" && position_2 == "O" && position_3 == "O")
           return win_combination
         end
-      
+
         end
       end
     end
