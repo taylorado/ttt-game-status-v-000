98f78f247e47d74b35ab16a3c69e70c2c42c7cfe
Taylor Donlan <taylordonlan@gmail.com>
2017-11-16 21:01:44 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -56,9 +56,7 @@ def draw?(board)
 end
 
 def over?(board)
-  if won?(board)
-    return true
-  elsif draw?(board)
+  if won?(board) || draw?(board) || !full?(board)
     return true
   else
     false
