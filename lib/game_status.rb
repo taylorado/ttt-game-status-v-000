802f9162642bd327e194dd51ecc8f359479e28da
Taylor Donlan <taylordonlan@gmail.com>
2017-11-17 00:13:39 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -28,11 +28,12 @@ def won?(board)
         position_3 = board[win_combination[2]]
         if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
 
-          return win_combination
+           win_combination
         elsif full?(board)
-           return false     # whats the difference between "else return false" and "else false" ???
-        else    false
+            false    
         end
+      else    false
+        
       end
     end
 end
