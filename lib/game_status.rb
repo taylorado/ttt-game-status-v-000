3ac401b86b04371d3d73b1e0d6a20a2f2e44d9f7
Taylor Donlan <taylordonlan@gmail.com>
2017-11-17 00:35:07 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -26,16 +26,13 @@ def won?(board)
         position_1 = board[win_combination[0]]
         position_2 = board[win_combination[1]]
         position_3 = board[win_combination[2]]
-        if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
-
-          return win_combination
-      #  elsif full?(board)
-      #     return false
-      #  else    false
-        end
+          if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
+            return win_combination
+          end
       end
+      false
     end
-    false
+#    false
 end
 
 
