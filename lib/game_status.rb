f9b45ec263ac01c59a053281c3516ef7ee254f0a
Taylor Donlan <taylordonlan@gmail.com>
2017-11-17 00:34:01 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -29,8 +29,8 @@ def won?(board)
         if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
 
           return win_combination
-        elsif full?(board)
-           return false
+      #  elsif full?(board)
+      #     return false
       #  else    false
         end
       end
